76dfa86747f9156217f1a17cd4af08528e518538
abubakarsiddique10 <abubakarsiddique7912@gmail.com>
2022-06-10 17:10:19 +0600

    implement backend in portfolio site

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://abu_bakar:lLgqinPaMfQ9mkOa@cluster0.w0cbn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const projectCollection = client.db("my_portfolio").collection('projects');
+
+        app.get('/projects', async (req, res) => {
+            const query = {};
+            const cursor = projectCollection.find(query);
+            const projects = await cursor.toArray();
+            res.send(projects)
+        })
+        app.get('/projects/:id', async (req, res) => {
+            console.log(req.params.id)
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await projectCollection.findOne(filter);
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('server running');
+})
+app.listen(port, () => {
+    console.log('app listen', port);
+})
