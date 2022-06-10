eecb8d8261833dcbaab7875b53fd01310fa8f6a0
abubakarsiddique10 <abubakarsiddique7912@gmail.com>
2022-06-10 17:16:25 +0600

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://abu_bakar:lLgqinPaMfQ9mkOa@cluster0.w0cbn.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.w0cbn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
